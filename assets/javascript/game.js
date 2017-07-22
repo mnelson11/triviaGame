dddc41fbab718425bedd1ad15592d3ba6ae1bde6
mnelson11 <mackenziernelson@gmail.com>
2017-07-22 11:57:21 -0400

    now with cat noises

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -41,7 +41,7 @@ window.onload= function(){
  	});
 
  	function totalScore(){
-
+ 	
  		clearInterval(intervalId);
 
  		var amountCorrect = 0;          
@@ -56,12 +56,28 @@ window.onload= function(){
  		}                   
     	$("#question").html("<div><h2> Total correct: "+amountCorrect+"/5</h2>");
 
+
     	if(amountCorrect>=4){
     		console.log("good job")
-    		$('#image').html('<img src="https://assets.rbl.ms/4142729/980x.jpg" alt="happy cat" style="width:600px"> <div><h3>Good Work!</h3></div> '); 
+    		$("<audio></audio>").attr({
+		    'src':'audio/Kitten-sounds/Kitten-sounds.mp3',
+		    'volume':0.4,
+		    'autoplay':'autoplay'
+		}).appendTo("body");
+    		$('#image').html('<img src="http://www.papercards.com/store/graphics/avanti/cd9417-happy-kitten-with-red-bow-christmas-card.jpg" alt="happy cat" style="width:600px"> <div><h3>Good Work!</h3></div> '); 
     	}else if (amountCorrect==3){
-    		$('#image').html('<img src="https://s-media-cache-ak0.pinimg.com/736x/7f/e4/46/7fe446d149960c373c0646bdab65d529--gatos-cats-ginger-cat.jpg" alt="meh cat" style="width:600px"> <div><h3>Meh, Try a little harder next time.</h3></div>'); 
+    		$('#image').html('<img src="https://s-media-cache-ak0.pinimg.com/736x/7f/e4/46/7fe446d149960c373c0646bdab65d529--gatos-cats-ginger-cat.jpg" alt="meh cat" style="width:600px"> <div><h3>Meh, Try a little harder next time.</h3></div>');
+    		$("<audio></audio>").attr({
+			    'src':'audio/Sad-cat/Sad-cat.mp3',
+			    'volume':0.4,
+			    'autoplay':'autoplay'
+			}).appendTo("body"); 
     	} else{
+    		$("<audio></audio>").attr({
+		    'src':'audio/Cat-hissing-sound/Cat-hissing-sound.mp3',
+		    'volume':0.4,
+		    'autoplay':'autoplay'
+		}).appendTo("body");
     		$('#image').html('<img src="http://static.boredpanda.com/blog/wp-content/uploads/2014/02/funny-wet-cats-8.jpg" alt="Angry cat" style="width:600px"> <div><h3>Did you even try?</h3></div>'); 
     	}
 
