35b06abbbb837027bbce6205bcda5bcce376c6d7
mnelson11 <mackenziernelson@gmail.com>
2017-07-22 12:46:54 -0400

    changed file organization

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -60,7 +60,7 @@ window.onload= function(){
     	if(amountCorrect>=4){
     		console.log("good job")
     		$("<audio></audio>").attr({
-		    'src':'audio/Kitten-sounds/Kitten-sounds.mp3',
+		    'src':'audio/Kitten-sounds.mp3',
 		    'volume':0.4,
 		    'autoplay':'autoplay'
 		}).appendTo("body");
@@ -68,13 +68,13 @@ window.onload= function(){
     	}else if (amountCorrect==3){
     		$('#image').html('<img src="https://s-media-cache-ak0.pinimg.com/736x/7f/e4/46/7fe446d149960c373c0646bdab65d529--gatos-cats-ginger-cat.jpg" alt="meh cat" style="width:600px"> <div><h3>Meh, Try a little harder next time.</h3></div>');
     		$("<audio></audio>").attr({
-			    'src':'audio/Sad-cat/Sad-cat.mp3',
+			    'src':'audio/Sad-cat.mp3',
 			    'volume':0.4,
 			    'autoplay':'autoplay'
 			}).appendTo("body"); 
     	} else{
     		$("<audio></audio>").attr({
-		    'src':'audio/Cat-hissing-sound/Cat-hissing-sound.mp3',
+		    'src':'audio/Cat-hissing-sound.mp3',
 		    'volume':0.4,
 		    'autoplay':'autoplay'
 		}).appendTo("body");
